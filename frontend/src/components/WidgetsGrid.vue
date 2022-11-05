0dbfb3d27891261e39432f46c3808badc9fb27b3
st-angelo <statescu.relu@gmail.com>
2022-11-06 01:55:50 +0200

    add vue-smart-widget library, add simple add widget functionality

---
+++ frontend/src/components/WidgetsGrid.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="grid-widget">
+    <smart-widget-grid :layout="layout">
+      <smart-widget
+        v-for="(widget, index) in widgets"
+        :slot="index"
+        :title="widget.title"
+        :sub-title="widget.subTitle"
+        :key="index"
+      >
+        <div class="layout-center">
+          <h3>{{ widget.content }}</h3>
+        </div>
+      </smart-widget>
+    </smart-widget-grid>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GridWidget',
+  props: {
+    widgets: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      layout: [
+        { x: 0, y: 0, w: 4, h: 4, i: '0' },
+        { x: 4, y: 0, w: 4, h: 4, i: '1' },
+        { x: 8, y: 0, w: 4, h: 4, i: '2' },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ frontend/src/components/addModal.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-dialog transition="dialog-top-transition" max-width="600">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn icon elevation="4" v-bind="attrs" v-on="on">
+        <v-icon> mdi-plus-thick </v-icon>
+      </v-btn>
+    </template>
+    <template v-slot:default="dialog">
+      <v-card>
+        <v-toolbar color="primary" dark>Add something</v-toolbar>
+        <v-card-text>
+          <div class="text-h2 pa-12">Hello world!</div>
+          <v-text-field v-model="input" label="Add something"></v-text-field>
+        </v-card-text>
+        <v-card-actions class="justify-end">
+          <v-btn
+            text
+            @click="
+              addWidget();
+              dialog.value = false;
+            "
+            >Add</v-btn
+          >
+          <v-btn text @click="dialog.value = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+    };
+  },
+  methods: {
+    addWidget() {
+      this.$emit('addWidget', this.input);
+      this.input = '';
+    },
+  },
+};
+</script>
