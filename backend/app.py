5a9e106fc30e36ac160bc786702fd23793107650
Licu Mihai <licu.mihai21@gmail.com>
2022-11-06 04:31:57 +0200

    api extension

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import datetime
-from mongoengine.fields import EmbeddedDocument, EmbeddedDocumentListField, ReferenceField, DictField, StringField, ListField, IntField, DateTimeField, ReferenceField
+from mongoengine.fields import EmbeddedDocument, EmbeddedDocumentListField, DictField, StringField, IntField, DateTimeField, ReferenceField
 from threading import Lock
 from mongoengine import Document, disconnect, connect
 from flask import Flask, request
@@ -23,8 +23,8 @@ class Scrape(EmbeddedDocument):
 
 class Source(Document):
     url = StringField()
-    interval = IntField()
-    last_scraped = DateTimeField()
+    interval = IntField(default=60*60)
+    last_scraped = DateTimeField(default=datetime.datetime.min)
     scrapes = EmbeddedDocumentListField(Scrape)
 
     def scrape_job(self):
@@ -71,7 +71,6 @@ mongoConnection = MongoConnection()
 class Scraper(Resource):
     def get(self):
         with mongoConnection:
-            expired_scrapes = []
             for source in Source.objects:
                 if source.lastscraped + datetime.timedelta(seconds=source.interval) < datetime.datetime.now():
                     continue
@@ -100,7 +99,13 @@ class Extension(Resource):
     def post(self):
         args = request.get_json()
         with mongoConnection:
-            
+            Source(
+                url=args["url"],
+                scrapes=[
+                    Scrape(name=scrape["name"], selector=scrape["selector"])
+                    for scrape in args["scrapes"]]
+            ).save()
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8069)
